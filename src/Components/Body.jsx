14eeb933ef57a857792f915733649fff0121b030
sumit9748 <95860567+sumit9748@users.noreply.github.com>
2023-11-09 15:53:30 +0530

    Initial commit

---
+++ src/Components/Body.jsx
@@ -0,0 +1,25 @@
+const Body = () => {
+  return (
+    <div className="body">
+      <h3 className="textleft">
+        <span
+          style={{
+            backgroundColor: "azure"
+          }}
+        >
+          This is a heading1
+        </span>
+      </h3>
+      <h3 className="textright">
+        <span
+          style={{
+            backgroundColor: "azure"
+          }}
+        >
+          This is a heading2
+        </span>
+      </h3>
+    </div>
+  );
+};
+export default Body;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className="Navbar">
+      <div className="left">Medicare</div>
+      <div className="right">Welcome Sumit</div>
+    </div>
+  );
+};
+export default Navbar;
